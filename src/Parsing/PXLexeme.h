fb1f5b0fb2996f4aa983d1cbb58239e8a1e435ec
Kevin Lindsey <kevin@pixate.com>
2014-02-27 00:52:17 -0600

    Move more properties and methods from PXStylesheetLexemem to PXLexeme

--- src/Parsing/PXLexeme.h
+++ src/Parsing/PXLexeme.h
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2012-present Pixate, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 //
 //  PXLexeme.h
 //  Protostyle
@@ -10,10 +26,17 @@
 
 @protocol PXLexeme <NSObject>
 
-@property (nonatomic) int type;
-@property (nonatomic, strong) NSString *text;
+@property (nonatomic, readonly) int type;
+@property (nonatomic, strong, readonly) NSString *text;
 @property (nonatomic, strong, readonly) NSString *name;
 
+@property (nonatomic, strong, readonly) id value;
+@property (nonatomic, readonly) NSRange range;
+
 - (id)initWithType:(int)type text:(NSString *)text;
+
+- (void)clearFlag:(int)type;
+- (void)setFlag:(int)type;
+- (BOOL)flagIsSet:(int)type;
 
 @end
