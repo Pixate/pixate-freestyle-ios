2d75379a6034e3a18ccdab99706ebe6f4676d4f2
Paul Colton <paul@pixate.com>
2014-02-27 20:48:36 -0800

    rearrange files moving towards 3.0 refactor

--- src/Version.h
+++ src/Version.h
@@ -25,7 +25,7 @@
 #ifndef Pixate_Version_h
 #define Pixate_Version_h
 
-#define PIXATE_FREESTYLE_VERSION "2.1.0"
+#define PIXATE_FREESTYLE_VERSION "2.1.1"
 
 #define PIXATE_FREESTYLE_API_VERSION 2
 
