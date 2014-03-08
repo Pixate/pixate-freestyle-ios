6a0624d223b9675a55a5271687e7a059e5f32d25
Paul Colton <paul@pixate.com>
2014-03-08 15:33:45 -0800

    uitableview test

---
+++ tests/UILabelTest/UILabelTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UILabelTest
+//
+//  Created by Paul Colton on 3/8/14.
+//  Copyright (c) 2014 Pixate, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
