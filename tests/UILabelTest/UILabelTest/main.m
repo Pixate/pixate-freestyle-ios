33017a8331e4890d1c8f804064314ab7db9d2c80
Paul Colton <paul@pixate.com>
2014-03-09 13:01:20 -0700

    updated label test

--- tests/UILabelTest/UILabelTest/main.m
+++ tests/UILabelTest/UILabelTest/main.m
@@ -9,10 +9,12 @@
 #import <UIKit/UIKit.h>
 
 #import "AppDelegate.h"
+#import "PixateFreestyle.h"
 
 int main(int argc, char * argv[])
 {
     @autoreleasepool {
+        [PixateFreestyle initializePixateFreestyle];
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
