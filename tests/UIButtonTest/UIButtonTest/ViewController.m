fdc9c226fca5b84684b9290e7f073af1d73d43f1
Paul Colton <paul@pixate.com>
2014-03-13 09:45:48 -0700

    add few more button tests

--- tests/UIButtonTest/UIButtonTest/ViewController.m
+++ tests/UIButtonTest/UIButtonTest/ViewController.m
@@ -18,9 +18,6 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    
-    [self.button setTitle:@"Testing" forState:UIControlStateNormal];
-    
 }
 
 - (void)didReceiveMemoryWarning
